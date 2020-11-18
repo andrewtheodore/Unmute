46ba698f2fc97fde51f2afb8f89bd89ac8618113
@andrewtheodore <andrewtheodoret@gmail.com>
2020-11-19 04:54:51 +0700

    Add password type for password field

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,7 @@
 import '../styles/globals.css'
-import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  <>
-    <Head>
-      <title>Coach Portal</title>
-      <link rel="shortcut icon" href="/logo_v1 1.png" />
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
